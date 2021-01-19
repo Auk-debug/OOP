c9bebc33aea9f34159e17d8b326cda565bcd49f7
Stein L. Tomassen <steint@ntnu.no>
2021-01-19 18:39:13 +0100

    Kodeeksempler for forelesning uke 3

---
+++ foreksempel/src/main/java/w03/Counter.java
@@ -0,0 +1,67 @@
+package w03;
+
+class Counter {
+	int end;
+	int counter = 1;
+
+	// Konstruktøren. Vi måtte ha inn hva end skulle være. counter skulle
+	// alltid starte på 1.
+	public Counter(int end) {
+		this.end = end;
+	}
+
+	int getCounter() {
+		return counter;
+	}
+
+	boolean count() {
+		if (counter == end)
+			return true;
+		else {
+			counter++; // samme som 'counter += 1' som er det samme som 'counter = counter + 1'
+			return false;
+		}
+	}
+
+	/**
+	 * Vi kan jo starte med å lage den akkurat som når en skal gå opp med 1.
+	 */
+	boolean count(int inc) {
+		if (counter == end)
+			return true;
+		else {
+			counter += inc;
+			return false;
+			// Ser dere hva som blir feil her? Kan dere fikse feilene selv?
+		}
+	}
+	/*
+	 * For øvrig kan en se på kravene slik de er beskrevet i lysarkene og spørre om
+	 * det er helt spesifikt beskrevet. Gitt tilstanden counter = 4, end = 5, og en
+	 * kaller count(2). Det er nettopp usikkerheten i beskrivelsen som gjør at det
+	 * blir litt vanskelig å lage ting uten at det også kan krasje.
+	 */
+
+	public static void main(String[] args) {
+		Counter c = new Counter(3);
+		System.out.println(c.getCounter());
+		System.out.println(c.count(2));
+		System.out.println(c.getCounter());
+		System.out.println(c.count(2));
+
+		// Her kan vi også lage en løkke som gås igjennom helt til vi er ferdige:
+		Counter c2 = new Counter(8);
+		while (c2.count() == false) {
+			System.out.println(c2.count());
+			System.out.println(c2.getCounter());
+		}
+
+		// Kan dere finne ut hva som er galt her, og fikse det?
+//		Counter c3 = new Counter(8);
+//		while (c3.count() == false) {
+//			System.out.println(c3.count(3));
+//			System.out.println(c3.getCounter());
+//		}
+	}
+
+}
